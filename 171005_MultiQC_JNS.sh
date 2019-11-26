4b9db7486a59bfaf4ee8a327157c28190ceeb02c
Jonas N. Søndergaard <jonas.sondergaard@ki.se>
2019-11-26 15:55:35 +0100

    script to run MultiQC
    script to run MultiQC

---
+++ 171005_MultiQC_JNS.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#script to run MultiQC
+#Made by Jonas N. Søndergaard
+#Made on 170928
+
+#UPPMAX commands (Uppsala Multidisciplinary Center for Advanced Computational Science)
+#SBATCH -A uppmax_proj_number 
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 4:00:00
+
+#load packages. bioinfo-tools is loaded on uppmax in order to load all other packages used.
+module load bioinfo-tools
+module load MultiQC/1.2
+
+#run MultiQC
+multiqc \
+	/proj/FQfiles
